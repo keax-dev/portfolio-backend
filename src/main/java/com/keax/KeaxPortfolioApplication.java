a1258f6e5f182e62d7010128d3fdd10d0a39378b
Kevin-Galarza77 <jimenezkev1040@gmail.com>
2025-08-06 16:23:07 -0500

    Project start

---
+++ src/main/java/com/keax/KeaxPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.keax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeaxPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeaxPortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keax/KeaxPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeaxPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
